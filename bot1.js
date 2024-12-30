bc04222c40905da2cc6b02a422c3d49990f9a615
Mohan0407 <97589281+Mohan0407@users.noreply.github.com>
2024-12-30 21:08:50 +0530

    Add files via upload

---
+++ bot1.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CSBS - Web Page</title>
+	<link rel="stylesheet" type="text/css" href="CSBS.css">
+</head>
+<body>
+<div id="div1">
+	<div id="div2">
+		<p id="id1">Computer Science and Business Systems</p>
+	</div>
+
+	<center>
+
+    	<div class="scroll-container">
+  			<img src="image1.jpg" alt="image1">
+  			<img src="image2.jpg" alt="image2">
+  			<img src="image3.jpg" alt="image3">
+  			<img src="image4.jpg" alt="image4">
+  			<img src="image5.jpg" alt="image5">
+		</div>
+
+	</center>
+
+	<div id="div4">
+		<p id="id2">About Us</p>
+		<div id="div8">
+			<p id="p1">To address the growing need of engineering talent with skills in digital technology, TCS, in partnership with leading academicians across India, has designed a curriculum for 4 years undergraduate program on Computer Science titled “Computer Science and Business Systems (CSBS)." This curriculum aims to ensure that the students graduating from the program not only know the core topics of Computer Science but also develop an equal appreciation of humanities, management sciences and human values. The students are also exposed to emerging topics such as Analytics, Machine Learning, Cloud Computing, Internet of Things etc. to make them industry ready at the end of four years of study.</p>
+
+
+		</div>
+	</div>
+
+	<div id="div5">
+		<p>Curriculum</p>
+	</div>
+
+	<div id="div6">
+		<p>Club Activities</p>
+	</div>
+
+	<div id="div7">
+		<p>Placements</p>
+	</div>
+
+
+	<hr>
+
+
+
+</div>
+</body>
+</html>
